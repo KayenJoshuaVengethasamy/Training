b4790947b417a81a4bc26eb9a5b6fe5be97a8b34
KayenJoshuaVengethasamy <kjvengethasamy@gmail.com>
2022-09-02 00:24:05 +0200

    Section 5: Java: Decision making under Java
    Udemy Full Stack Java developer - Java + JSP + Restful WS + Spring

---
+++ Udemy/Full_Stack_Java_Developer_Java_JSP_ Restful WS_Spring/Lessons/S04L01_Arithmetic_Operators/src/org/training/Hello.java
@@ -0,0 +1,32 @@
+package org.training;
+
+public class Hello {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 1 + 2;
+		String c = "Hello" + "World";
+		int d = 12 - 1;
+		int e = 12 * 2;
+		int f = 12 / 2;
+		int g = 13 % 2;
+		int h = 10;
+		int i = 10;
+		System.out.println("a : " + a);
+		System.out.println("b : " + b);
+		System.out.println("c : " + c);
+		System.out.println("d : " + d);
+		System.out.println("e : " + e);
+		System.out.println("f : " + f);
+		System.out.println("g : " + g);
+		System.out.println("h : " + h);
+		System.out.println("++h : " + ++h);
+		System.out.println("h++ : " + h++);
+		System.out.println("h : " + h);
+		System.out.println("i : " + i);
+		System.out.println("i-- : " + i--);
+		System.out.println("--i : " + --i);
+		System.out.println("i : " + i);
+
+	}
+
+}
