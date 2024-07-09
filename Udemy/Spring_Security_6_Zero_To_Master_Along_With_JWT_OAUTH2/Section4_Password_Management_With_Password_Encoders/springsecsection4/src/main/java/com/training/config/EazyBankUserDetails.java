e3f522acff2c9cd8c594ba062978c299f99645c1
KayenJoshuaVengethasamy <kjvengethasamy@gmail.com>
2024-07-09 16:21:14 +0200

    Section_4_Password_Management_With_Password_Encoders
    Spring Security 6. SpringBoot 3 security, CORs,CSRF, JWT, OAUTH2,OpenID Connect, Keycloak

---
+++ Udemy/Spring_Security_6_Zero_To_Master_Along_With_JWT_OAUTH2/Section4_Password_Management_With_Password_Encoders/springsecsection4/src/main/java/com/training/config/EazyBankUserDetails.java
@@ -0,0 +1,49 @@
+package com.training.config;
+
+import com.training.model.Customer;
+import com.training.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EazyBankUserDetails implements UserDetailsService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+    /**
+     * Locates the user based on the username. In the actual implementation, the search
+     * may possibly be case sensitive, or case insensitive depending on how the
+     * implementation instance is configured. In this case, the <code>UserDetails</code>
+     * object that comes back may have a username that is of a different case than what
+     * was actually requested..
+     *
+     * @param username the username identifying the user whose data is required.
+     * @return a fully populated user record (never <code>null</code>)
+     * @throws UsernameNotFoundException if the user could not be found or the user has no
+     *                                   GrantedAuthority
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String userName, password = null;
+        List<GrantedAuthority> authorities = null;
+        List<Customer> customer = customerRepository.findByEmail(username);
+        if(customer.size() == 0){
+            throw new UsernameNotFoundException("User details not found for the user : " + username);
+        }else{
+            username = customer.get(0).getEmail();
+            password = customer.get(0).getPwd();
+            authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+        }
+        return new User(username, password,authorities);
+    }
+}
